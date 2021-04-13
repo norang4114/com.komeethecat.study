3d26c10c27511f97313a104ba060830e5658c903
Komeethecat <Komeethecat@gmail.com>
2021-04-13 22:21:12 +0900

    HelloControllerTest 오류

---
+++ src/test/java/com/kommethecat/study/springboot/web/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package com.kommethecat.study.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+        mvc.perform(get("/hello")).andExpect(status().isOK()).andExpect(content().string(hello));
+
+    }
+}
